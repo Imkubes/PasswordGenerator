ac20c7dead0356a97624ef619a3898f5804f3e5a
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-03-23 10:16:07 -0500

    checking generatePassword function

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,10 @@ function writePassword() {
 
 }
 
+function generatePassword(){
+  return "password";
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
