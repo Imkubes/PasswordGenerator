96f83f585fb979383f4cb63f957d065b2396582a
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-03-24 00:03:37 -0500

    added specialCharacters variable

--- assets/script.js
+++ assets/script.js
@@ -43,11 +43,13 @@ generateBtn.addEventListener("click", writePassword);
 
 // Transforming the array into a string -- join method
 
+
+//Prompting user for length desired for password
 var passwordLength = prompt("How many characters would you like in your password? Must be a minimum of 8 characters and a maximum of 128 characters", "");
 console.log(passwordLength);
-
+//Converting string to numeric data type
 passwordLength = parseInt(passwordLength);
-
+//else if statement making sure the user cannot enter regular characters, must be >= 8 <=128
 if (isNaN(passwordLength)) {
   alert("You did not enter a valid number");
 } else if (passwordLength < 8) {
@@ -59,3 +61,13 @@ if (isNaN(passwordLength)) {
 }
 
 console.log(typeof passwordLengthNumber);
+
+var specialCharacters = confirm("Would you like to include special character's? Press Ok for yes, Cancel for no.");
+
+if (specialCharacters == true) {
+  specialCharacters = true;
+  console.log(specialCharacters);
+} else {
+  specialCharacters = false;
+  console.log(specialCharacters);
+}
